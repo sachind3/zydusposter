b58bd70064c16ac0d45df3cfd09e9cf77cd890e8
sachind3 <desai.sachin45@gmail.com>
2023-10-05 22:52:32 +0530

    added poster 7

--- src/lib/templates.js
+++ src/lib/templates.js
@@ -23,17 +23,23 @@ const templates = [
   //   poster_name: "poster 4",
   //   lang: ["english"],
   // },
+  // {
+  //   id: "5",
+  //   poster_path: "poster-5",
+  //   poster_name: "poster 5",
+  //   lang: ["english"],
+  // },
+  // {
+  //   id: "6",
+  //   poster_path: "poster-6",
+  //   poster_name: "poster 6",
+  //   lang: ["english"],
+  // },
   {
-    id: "5",
-    poster_path: "poster-5",
-    poster_name: "poster 5",
-    lang: ["english"],
-  },
-  {
-    id: "6",
-    poster_path: "poster-6",
-    poster_name: "poster 6",
-    lang: ["english"],
+    id: "7",
+    poster_path: "poster-7",
+    poster_name: "poster 7",
+    lang: ["english", "hindi"],
   },
 ];
 export default templates;
